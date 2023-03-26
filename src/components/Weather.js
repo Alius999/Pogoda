64beb5066de869499f1aad097a5e681f83eefc44
Alius999 <myzikant999@gmail.com>
2023-03-26 17:37:25 +0500

    Finish

---
+++ src/components/Weather.js
@@ -0,0 +1,20 @@
+// const axios = require('axios').default;
+
+const Ber = () => {
+
+    // axios.get('https://api.open-meteo.com/v1/forecast?latitude=51.77&longitude=55.10&hourly=temperature_2m')
+    //     .then(response => {
+    //         console.log(response);
+    //     })
+    //     .catch((error) => {
+    //         console.error(error)
+    //     })
+
+    return(
+        <div>
+            <p>Привет я Рекат приложение погоды!</p>
+        </div>
+    )
+}
+
+export default Ber;
